aac6214318ecfa591ae2707701e40aa967a20f18
Job <3dsphere@Jobs-MacBook-Air.local>
2021-11-15 14:26:01 -0800

    created Papers component

---
+++ frontend/arxiv/src/components/Papers.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+
+const Papers = (props) => {
+    const {papers} = props;
+    if (!papers || papers.length === 0) return <p>Cannot find any papers, sorry</p>;
+    return (
+        <React.Fragment>
+            {papers.map((paper) => {
+                return (
+                    <Grid item key={paper.title} xs={12} md={4}>
+                        <h2>{paper.title}</h2>
+                        <p>{paper.abstract}</p>
+
+                    </Grid>
+
+                )
+            }
+            )}
+         </React.Fragment>
+    );
+};
+
+export default Papers;
