0267c4c924fa7cf12c7c4563670f9814f524dc8c
Job <3dsphere@Jobs-MacBook-Air.local>
2021-11-15 14:25:16 -0800

    created initial logic for App,js

--- frontend/arxiv/src/App.js
+++ frontend/arxiv/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Papers from './components/Papers';
+
+const apiUrl = "http://127.0.0.1:8000/api";
 
 function App() {
+  const [apiData, setApiData] = useState({});
+
+  useEffect(() => {
+    getBioArxivWithFetch();
+  }, []);
+
+  const getBioArxivWithFetch = async () => {
+    const response = await fetch(apiUrl);
+    const jsonData = await response.json();
+    setApiData(jsonData);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Latest Papers</h1>
+      <Papers papers={apiData.papers}/>
     </div>
-  );
+  )
 }
+
 
 export default App;
