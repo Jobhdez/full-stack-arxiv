cb130c928f070185a2bffb7ec4a7f821b85c701f
Job Hernandez <hj93@protonmail.com>
2022-01-13 11:18:30 -0800

    changed color of title

--- frontend/arxiv/src/components/ResearchPaperCard.js
+++ frontend/arxiv/src/components/ResearchPaperCard.js
@@ -1,15 +1,23 @@
 import * as React from 'react';
+import { makeStyles } from '@mui/styles';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 
+const useStyles = makeStyles({
+  root: {
+    color: 'blue'
+  },
+});
+
 const ResearchPaperCard = ({title, abstract}) => {
+  const colorClass = useStyles();
 
     return (
       <div>
       <Card sx={{display: 'flex'}}>
         <CardContent>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" component="div">
+          <Typography sx={{ fontSize: 14 }} className={colorClass.root} component="div">
             {title}
           </Typography>
           
