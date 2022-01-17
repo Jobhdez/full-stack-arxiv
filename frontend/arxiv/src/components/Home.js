ca3a9fcf5465c76c6eb3b9e8afa765bab94cc399
Job Hernandez <hj93@protonmail.com>
2022-01-17 10:19:03 -0800

    organized files

---
+++ frontend/arxiv/src/components/Home.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import Grid from '@mui/material/Grid';
+import PaperCard from './ResearchPaperCard';
+
+const apiUrl = "http://127.0.0.1:8000/api/";
+
+function Home() {
+  const [apiData, setApiData] = useState([])
+  const [visible, setVisible] = useState(3);
+
+  const loadMorePapers = () => {
+    setVisible((prevValue) => prevValue + 3);
+  }
+  useEffect(() => {
+    getBioArxivWithFetch();
+  },[]);
+
+  const getBioArxivWithFetch = async () => {
+    const response = await fetch(apiUrl);
+    const jsonData = await response.json();
+    console.log(jsonData);
+    setApiData(jsonData);
+
+  };
+  console.log(apiData);
+  console.log(apiData[0])
+  return (
+    <div>
+      <div style={{display:'flex', justifyContent:'center'}}>
+      <h1>Latest Papers</h1>
+      </div>
+      {apiData.slice(0, visible).map((paper) => (
+        <Grid key={paper.title}>
+          <PaperCard title={paper.title} abstract={paper.abstract}/>
+        </Grid>
+      ))}
+      <div style={{display:'flex', justifyContent: 'center'}}>
+      <button onClick={loadMorePapers}>Load More</button>
+      </div>
+      </div>
+        
+  )
+      }
+      
+
+
+export default Home;
