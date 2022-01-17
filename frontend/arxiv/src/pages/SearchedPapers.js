6979c32814a2ae74c75f984a4d2269098a370437
Job Hernandez <hj93@protonmail.com>
2022-01-17 10:20:32 -0800

    mostly solved the search functionality (page)

--- frontend/arxiv/src/pages/SearchedPapers.js
+++ frontend/arxiv/src/pages/SearchedPapers.js
@@ -4,42 +4,50 @@ import PaperCard from '../components/ResearchPaperCard';
 
 const apiUrl = "http://127.0.0.1:8000/api/search/?search=";
 
-const getFilteredItems = async (query) => {
-
-    let response = await fetch(apiUrl+query);
-    let papers = await response.json();
-
-    if (!query) {
-        return <h1 Keep looking></h1>
-    }
-    return {papers};
-    
-
-}
 
 function SearchedPapers(){
 
     const [query, setQuery] = useState("");
+    const [apiData, setApiData] = useState([])
 
-    const filteredItems = getFilteredItems(query);
+    useEffect(() => {
+        const getFilteredItems = async (query) => {
+             let response = await fetch(apiUrl+query);
+             let papers = await response.json();
+             setApiData(papers);
+             if (!query) {
+                return papers
+            }
+            return papers;
+            
+        
+        }
+    
+        getFilteredItems(query);
+      },[query]);
 
+    
+    console.log(apiData)
     return (
         <div className='SearchedPapers'>
             <label>
                 Search
             </label>
-            <input type='text' onChange={(e) => setQuery(e.target.value)}>
-                <ul>
-                    {Object.values(filteredItems).map((paper) => (
+            <input type='text' onChange={e => setQuery(e.target.value)}/>
+            <div>
+                
+                    {apiData.map((paper) => {
+                        return (
                     <Grid key={paper.title}>
                         <PaperCard title={paper.title} abstract={paper.abstract}/>
                         </Grid>
-      ))} 
-                </ul>
-            </input>
+                        )
+                    })}
+            </div>
+                
+
         </div>
     )
-
-}
+                }
 
 export default SearchedPapers;
