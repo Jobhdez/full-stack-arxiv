026d9654a23d4ae935261ede5ecb8364963ea87b
Job Hernandez <hj93@protonmail.com>
2022-01-17 10:18:10 -0800

    organized into pages

--- frontend/arxiv/src/index.js
+++ frontend/arxiv/src/index.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Search from './pages/SearchedPapers';
 
-const routing = (
-	<Router>
-		<React.StrictMode>
-			<Header />
-			<Routes>
-				<Route exact path="/" element={<App />} />
-				<Route path='/SearchedPapers' element={<Search/>}/>
-			</Routes>
-			<Footer />
-		</React.StrictMode>
-	</Router>
-);
 
-ReactDOM.render(routing, document.getElementById('root'));
+ReactDOM.render(
+	<React.StrictMode>
+		<App/>
+	</React.StrictMode>,
+	document.getElementById('root'));
