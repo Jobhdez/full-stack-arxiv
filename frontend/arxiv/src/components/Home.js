c6a90043d3b4e74ab0c1720132c13f3ec7602b03
Job Hernandez <hj93@protonmail.com>
2022-01-25 22:24:57 -0800

    fixed the button state problem.

--- frontend/arxiv/src/components/Home.js
+++ frontend/arxiv/src/components/Home.js
@@ -1,14 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
 import PaperCard from './ResearchPaperCard';
 
 const apiUrl = "http://127.0.0.1:8000/api/";
 
+function LikeButton({title, abstract}) {
+  const [likes, setLikes] = useState(0);
+
+  const incrementLikes = () => {
+    const addToLikes = likes + 1;
+    setLikes(addToLikes);
+  };
+  return (
+    <div key={title}>
+      <Button variant="contained" onClick={incrementLikes}>Likes: {likes}</Button>
+      <PaperCard title={title} abstract={abstract}/>
+    </div>
+  );
+
+}
+
 function Home() {
   const [apiData, setApiData] = useState([])
   const [visible, setVisible] = useState(3);
-  const [likes, setLikes] = useState(0);
 
 
 
@@ -26,10 +40,7 @@ function Home() {
     setApiData(jsonData);
 
   };
-  const incrementLikes = () => {
-    const addToLikes = likes + 1;
-    setLikes(addToLikes)
-    }
+  
 
   return (
     <div>
@@ -37,10 +48,9 @@ function Home() {
       <h1>Latest Papers</h1>
       </div>
       {apiData.slice(0, visible).map((paper) => (
-        <Grid key={paper.title}>
-          <button onClick={incrementLikes}>Likes: {likes}</button>
-          <PaperCard title={paper.title} abstract={paper.abstract}/>
-        </Grid>
+        
+          <LikeButton title={paper.title} abstract={paper.abstract} key={paper.title}/>
+      
       ))}
       <div style={{display:'flex', justifyContent: 'center'}}>
       <Button variant="contained" onClick={loadMorePapers}>Load More</Button>
